9efc6e0ca64eb56d4961a45c4ee3cbf011973053
ChenWes <chenxuhua0530@163.com>
2017-11-01 14:56:35 +0800

    change project text
    * change project text
    * change project splash image

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -8,7 +8,7 @@ import { NavController, NavParams } from 'ionic-angular';
 export class ListPage {
   selectedItem: any;
   icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  items: Array<{ title: string, note: string, icon: string }>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     // If we navigated to this page, we will have an item available as a nav param
@@ -16,14 +16,14 @@ export class ListPage {
 
     // Let's populate this page with some filler content for funzies
     this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
+      'american-football', 'boat', 'bluetooth', 'build'];
 
     this.items = [];
     for (let i = 1; i < 11; i++) {
       this.items.push({
         title: 'S17GS000' + i,
         note: 'SJO#' + i,
-        icon: this.icons[i-1]
+        icon: this.icons[i - 1]
       });
     }
   }
